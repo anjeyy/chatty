6de346c2be175dc7104c2e613ea5a82587f06519
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2021-11-12 13:52:42 +0100

    project hull

---
+++ client/src/main/java/com/github/anjeyy/client/api/EntryPoint.java
@@ -0,0 +1,17 @@
+package com.github.anjeyy.client.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EntryPoint implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(EntryPoint.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Raw {}", args);
+    }
+}
