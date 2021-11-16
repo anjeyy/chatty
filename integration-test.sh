1ed3bc7bc24aa8891b9930a91702d8e0d1833329
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2021-11-16 16:25:25 +0100

    trigger pipeline

--- integration-test.sh
+++ integration-test.sh
@@ -3,11 +3,32 @@
 set -e
 
 
-### connect two clients and display message
-
+### connect two clients and display message ###
+# create server
 docker run -d -p 8080:8080 --name chatty-server anjeyy/chatty:server-latest
 sleep 10s
 docker logs chatty-server
-docker run -d --name chatty-client --network="host" -it anjeyy/chatty:client-latest
+# create first client
+docker run -d --name chatty-client-one --network="host" -it anjeyy/chatty:client-latest
 sleep 10s
-docker logs chatty-client
+docker logs chatty-client-one
+# create second client
+docker run -d --name chatty-client-two --network="host" -it anjeyy/chatty:client-latest
+sleep 10s
+docker logs chatty-client-two
+
+# simulate user input
+docker exec chatty-client-one echo "automated user 1" | socat EXEC:"docker attach chatty-client-one",pty STDIN
+docker logs chatty-client-one
+
+docker exec chatty-client-two echo "automated user 2" | socat EXEC:"docker attach chatty-client-two",pty STDIN
+docker logs chatty-client-two
+
+sleep 5s
+
+docker exec chatty-client-one echo "automated message - yeah" | socat EXEC:"docker attach chatty-client-one",pty STDIN
+docker logs chatty-client-one
+
+sleep 1s
+
+docker logs chatty-client-two
