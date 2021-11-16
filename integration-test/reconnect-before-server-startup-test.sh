eaf87872357ed10a14b5e0f9de8951c5f1a4ae5d
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2021-11-16 17:24:48 +0100

    trigger pipeline

--- integration-test/reconnect-before-server-startup-test.sh
+++ integration-test/reconnect-before-server-startup-test.sh
@@ -17,7 +17,7 @@ function createAndStartServer() {
 
 function createAndStartClient() {
   DOCKER_NAME=$1
-  docker run -d --name "$DOCKER_NAME" --network="host" -it anjeyy/chatty:client-latest
+  docker run -d -e websocket.retry_timeout=1000 --name "$DOCKER_NAME" --network="host" -it anjeyy/chatty:client-latest
   sleep 10s
   docker logs "$DOCKER_NAME"
 }
@@ -52,17 +52,19 @@ function verifySentMessage() {
 ### main ###
 ############
 
-createAndStartServer
 createAndStartClient chatty-client-one
-createAndStartClient chatty-client-two
 
-setUsernameForClient "automated user 1" chatty-client-one
-setUsernameForClient "automated user 2" chatty-client-two
-sleep 5s
+setUsernameForClient "automated user 1" chatty-client
 
-sendMessageViaClient "automated message from first client - yeah" chatty-client-one
-sendMessageViaClient "automated message from second client - double yeah" chatty-client-two
-sleep 1s
+sleep 10s
+docker logs chatty-client
 
-verifySentMessage " automated user 1~ automated message from first client - yeah" chatty-client-two
-verifySentMessage " automated user 2~ automated message from second client - double yeah" chatty-client-one
+#setUsernameForClient "automated user 2" chatty-client-two
+#sleep 5s
+#
+#sendMessageViaClient "automated message from first client - yeah" chatty-client-one
+#sendMessageViaClient "automated message from second client - double yeah" chatty-client-two
+#sleep 1s
+#
+#verifySentMessage " automated user 1~ automated message from first client - yeah" chatty-client-two
+#verifySentMessage " automated user 2~ automated message from second client - double yeah" chatty-client-one
