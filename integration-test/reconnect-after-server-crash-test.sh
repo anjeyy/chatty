cfcb86173a7fd8a66fbbb7215bef58ee2de18f1d
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2021-11-16 18:27:08 +0100

    trigger pipeline

--- integration-test/reconnect-after-server-crash-test.sh
+++ integration-test/reconnect-after-server-crash-test.sh
@@ -66,6 +66,7 @@ verifyMessage "~~ Connection to the Chatroom lost. ~~" chatty-client
 verifyMessage "~~ Enter a message trying to reconnect and send that message. ~~" chatty-client
 sendMessageViaClient "trigger reconnect" chatty-client
 docker start chatty-server
+sleep 5s
 
 # then
 docker logs chatty-client
@@ -74,4 +75,6 @@ verifyMessage "Waiting 2 s for another retry.. (1/5)" chatty-client
 verifyMessage "Waiting 2 s for another retry.. (2/5)" chatty-client
 verifyMessage "Waiting 2 s for another retry.. (3/5)" chatty-client
 verifyMessage "Waiting 2 s for another retry.. (4/5)" chatty-client
+verifyMessage "~~ Connection to the Chatroom established. ~~" chatty-client
+verifyMessage "~~ Say hi to the others. ~~" chatty-client
 
